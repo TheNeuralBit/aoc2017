b5cce7d461c49e3aabff3061514c0b1a3d5a18b3
Brian Hulette <brian.hulette@ccri.com>
2017-12-15 11:12:54 -0500

    Runtime from 27s -> 4s with numba.jit. Thanks kaharrs2!

--- 15/sol2.py
+++ 15/sol2.py
@@ -1,23 +1,22 @@
 from itertools import islice
+from numba import jit
 
 a_factor = 16807
 b_factor = 48271
 
-def generate(start, factor):
+@jit
+def generate(start, factor, mod):
     # 2147483647 = (2^31) - 1 = 0x7FFFFFFF
     n = start
     while True:
         n = ( n * factor ) % 0x7FFFFFFF
-        yield n
+        if n % mod == 0: yield n
 
 def generate_pairs(a_start, b_start, num):
-    a_generator = (a for a in generate(a_start, a_factor) if a % 4 == 0)
-    b_generator = (b for b in generate(b_start, b_factor) if b % 8 == 0)
-    yield from islice(zip(a_generator, b_generator), num)
+    return islice(zip(generate(a_start, a_factor, 4), generate(b_start, b_factor, 8)), num)
 
-assert list(islice(generate(65, a_factor), 5)) == [1092455, 1181022009, 245556042, 1744312007, 1352636452]
-assert list(islice(generate(8921, b_factor), 5)) == [430625591, 1233683848, 1431495498, 137874439, 285222916]
-
+assert list(islice(generate(65, a_factor, 1), 5)) == [1092455, 1181022009, 245556042, 1744312007, 1352636452]
+assert list(islice(generate(8921, b_factor, 1), 5)) == [430625591, 1233683848, 1431495498, 137874439, 285222916]
 
 a_start = 699
 b_start = 124
